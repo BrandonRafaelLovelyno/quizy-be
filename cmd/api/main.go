e354002de84e093d0efa4bc0813639e32f925ef7
Brandon <brandonrafael80@gmail.com>
2025-04-26 14:03:40 +0700

    integrate mongodb

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,18 +1,39 @@
 package main
 
 import (
+	"log"
 	"net/http"
-	middleware "quizy-be/internal"
+	"os"
+
+	"quizy-be/internal/db"
+	"quizy-be/internal/middleware"
 	"quizy-be/internal/router"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	r := chi.NewRouter()
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found - using system environment variables")
+	}
 
+	mongoURI := os.Getenv("MONGO_URI")
+	if mongoURI == "" {
+		log.Fatal("MONGO_URI environment variable not set")
+	}
+
+	if err := db.InitMongo(mongoURI); err != nil {
+		log.Fatalf("Failed to connect to MongoDB: %v", err)
+	}
+	defer db.DisconnectMongo()
+
+	r := chi.NewRouter()
 	middleware.SetupMiddleware(r)
 	router.SetupRouter(r)
 
-	http.ListenAndServe(":8080", r)
+	log.Println("Server starting on :8080")
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		log.Fatalf("Server failed to start: %v", err)
+	}
 }
